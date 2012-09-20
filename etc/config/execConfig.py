1a3db1efaf8428efe60c94fe92c530a5cc8c5a38
Stephen R. Pietrowicz <srp@ncsa.illinois.edu>
2012-09-20 18:57:37 -0500

    Adding files for gordon

---
+++ etc/config/execConfig.py
@@ -0,0 +1,4 @@
+root.platform.localScratch = "$USER_HOME/condor_logs"
+root.platform.defaultRoot = "/oasis/scratch/$USER_NAME/temp_project/defaultRoot"
+root.platform.dataDirectory = "/oasis/projects/nsf/nsa101/srp/inputdata/Sept2012/input"
+root.platform.fileSystemDomain = "sdsc.edu"
