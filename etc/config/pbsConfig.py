634f78f433abd747578c2299863021f8741b8128
Stephen R. Pietrowicz <srp@ncsa.illinois.edu>
2012-09-24 17:51:53 -0500

    Added pbs templates and config for allocateNodes command moved templates to "setups" subdirectory.

---
+++ etc/config/pbsConfig.py
@@ -0,0 +1,4 @@
+root.platform.queueName = "normal"
+root.platform.emailNotification = True
+root.platform.scratchDirectory = "$USER_HOME/condor_scratch"
+root.platform.loginHostName = "gordon.sdsc.edu"
