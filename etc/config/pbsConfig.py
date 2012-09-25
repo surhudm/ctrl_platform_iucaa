6f0cf1baa671bcccf2ec0fd4f1367d603c0c7463
Stephen R. Pietrowicz <srp@ncsa.illinois.edu>
2012-09-25 14:36:55 -0500

    Added glidein template Added email notification Changed job name from default (the script that submitted it) to the NODE_SET to make it easier to identify when you do a "qstat".

--- etc/config/pbsConfig.py
+++ etc/config/pbsConfig.py
@@ -1,5 +1,5 @@
 root.platform.queue = "normal"
-root.platform.email = True
+root.platform.email = "#PBS -m be"
 root.platform.scratchDirectory = "$USER_HOME/condor_scratch"
 root.platform.loginHostName = "gordon.sdsc.edu"
 root.platform.utilityPath = "/opt/torque/bin"
