bebf0ce5c266972bb6feb4568b147bf632707524
Stephen R. Pietrowicz <srp@ncsa.illinois.edu>
2012-09-26 15:25:36 -0500

    Renamed defaultRoot

--- etc/config/execConfig.py
+++ etc/config/execConfig.py
@@ -1,4 +1,4 @@
 root.platform.localScratch = "$HOME/condor_scratch"
-root.platform.defaultRoot = "/oasis/scratch/$USER_NAME/defaultRoot"
+root.platform.defaultRoot = "/oasis/scratch/$USER_NAME/temp_project/defaultRoot"
 root.platform.dataDirectory = "/oasis/projects/nsf/nsa101/srp/inputdata/Sept2012/input"
 root.platform.fileSystemDomain = "sdsc.edu"
