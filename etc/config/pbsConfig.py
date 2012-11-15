2238486ea7161449e6287191e2f2e338035cf7d7
Stephen R. Pietrowicz <srp@ncsa.illinois.edu>
2012-11-15 16:03:08 -0600

    Add support for configurable glidein shutdown times

--- etc/config/pbsConfig.py
+++ etc/config/pbsConfig.py
@@ -3,3 +3,4 @@ root.platform.email = "#PBS -m be"
 root.platform.scratchDirectory = "$USER_HOME/condor_scratch"
 root.platform.loginHostName = "gordon.sdsc.edu"
 root.platform.utilityPath = "/opt/torque/bin"
+root.platform.glideinShutdown = 3600
