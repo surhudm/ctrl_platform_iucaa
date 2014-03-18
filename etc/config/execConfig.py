db61cd42f4eeddbb319435f76f25de9c6c36c6a6
Stephen R. Pietrowicz <srp@ncsa.illinois.edu>
2014-03-18 16:07:07 -0500

    Added nodeset declaration to indicate its requirement

--- etc/config/execConfig.py
+++ etc/config/execConfig.py
@@ -1,3 +1,4 @@
+root.platform.nodeSetRequired = True
 root.platform.localScratch = "$HOME/condor_scratch"
 root.platform.defaultRoot = "/oasis/scratch/$USER_NAME/temp_project/defaultRoot"
 root.platform.dataDirectory = "/oasis/projects/nsf/nsa101/srp/inputdata/Sept2012/input"
